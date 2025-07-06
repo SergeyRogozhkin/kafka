5e1e2916bb3d993ddf2eeb720a6291a5b8cd9bd0
Sergey Rogozhkin <r8damik@gmail.com>
2025-07-06 15:40:44 +0400

    1 com

---
+++ Orders/src/main/java/org/example/orders/OrdersApplication.java
@@ -0,0 +1,16 @@
+package org.example.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdersApplication.class, args);
+		System.out.println("s");
+		System.out.println("s");
+		System.out.println("s");
+	}
+
+}
